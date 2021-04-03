ff1a37c62b764098635ca6a91d0a54c04a354028
Joceline Quisimalin <78117452+Joceline0814@users.noreply.github.com>
2021-04-02 22:54:56 -0500

    Add files via upload

---
+++ Examen_Grupo3/Grupo3_RentaPeliculas/Form1.Designer.vb
@@ -0,0 +1,184 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Presentacion
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Presentacion))
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.LBL_EJERCICIOS = New System.Windows.Forms.Label()
+        Me.LBL_NRC = New System.Windows.Forms.Label()
+        Me.LBL_INGENIERO = New System.Windows.Forms.Label()
+        Me.LBL_REDES = New System.Windows.Forms.Label()
+        Me.LBL_PROGRAMACION = New System.Windows.Forms.Label()
+        Me.LBL_JOSS = New System.Windows.Forms.Label()
+        Me.LBL_TECNOLOGIAS = New System.Windows.Forms.Label()
+        Me.LBL_UNIVERSIDAD = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.Button2.Font = New System.Drawing.Font("Times New Roman", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(739, 289)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(94, 37)
+        Me.Button2.TabIndex = 28
+        Me.Button2.Text = "SALIR"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.Button1.Font = New System.Drawing.Font("Times New Roman", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(739, 332)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(94, 37)
+        Me.Button1.TabIndex = 27
+        Me.Button1.Text = "SIGUIENTE"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'LBL_EJERCICIOS
+        '
+        Me.LBL_EJERCICIOS.AutoSize = True
+        Me.LBL_EJERCICIOS.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.LBL_EJERCICIOS.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LBL_EJERCICIOS.Location = New System.Drawing.Point(431, 350)
+        Me.LBL_EJERCICIOS.Name = "LBL_EJERCICIOS"
+        Me.LBL_EJERCICIOS.Size = New System.Drawing.Size(79, 19)
+        Me.LBL_EJERCICIOS.TabIndex = 26
+        Me.LBL_EJERCICIOS.Text = "EXAMEN"
+        '
+        'LBL_NRC
+        '
+        Me.LBL_NRC.AutoSize = True
+        Me.LBL_NRC.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.LBL_NRC.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LBL_NRC.Location = New System.Drawing.Point(429, 316)
+        Me.LBL_NRC.Name = "LBL_NRC"
+        Me.LBL_NRC.Size = New System.Drawing.Size(81, 19)
+        Me.LBL_NRC.TabIndex = 25
+        Me.LBL_NRC.Text = "NRC:4101"
+        '
+        'LBL_INGENIERO
+        '
+        Me.LBL_INGENIERO.AutoSize = True
+        Me.LBL_INGENIERO.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.LBL_INGENIERO.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LBL_INGENIERO.Location = New System.Drawing.Point(399, 279)
+        Me.LBL_INGENIERO.Name = "LBL_INGENIERO"
+        Me.LBL_INGENIERO.Size = New System.Drawing.Size(137, 19)
+        Me.LBL_INGENIERO.TabIndex = 24
+        Me.LBL_INGENIERO.Text = "ING. CAIZA JOSÉ"
+        '
+        'LBL_REDES
+        '
+        Me.LBL_REDES.AutoSize = True
+        Me.LBL_REDES.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.LBL_REDES.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LBL_REDES.Location = New System.Drawing.Point(332, 248)
+        Me.LBL_REDES.Name = "LBL_REDES"
+        Me.LBL_REDES.Size = New System.Drawing.Size(268, 19)
+        Me.LBL_REDES.TabIndex = 23
+        Me.LBL_REDES.Text = "REDES Y TELECOMUNICACIONES"
+        '
+        'LBL_PROGRAMACION
+        '
+        Me.LBL_PROGRAMACION.AutoSize = True
+        Me.LBL_PROGRAMACION.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.LBL_PROGRAMACION.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LBL_PROGRAMACION.Location = New System.Drawing.Point(362, 196)
+        Me.LBL_PROGRAMACION.Name = "LBL_PROGRAMACION"
+        Me.LBL_PROGRAMACION.Size = New System.Drawing.Size(207, 38)
+        Me.LBL_PROGRAMACION.TabIndex = 22
+        Me.LBL_PROGRAMACION.Text = "                GRUPO 3" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "PROGRAMACIÓN VISUAL"
+        '
+        'LBL_JOSS
+        '
+        Me.LBL_JOSS.AutoSize = True
+        Me.LBL_JOSS.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.LBL_JOSS.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LBL_JOSS.Location = New System.Drawing.Point(221, 126)
+        Me.LBL_JOSS.Name = "LBL_JOSS"
+        Me.LBL_JOSS.Size = New System.Drawing.Size(497, 57)
+        Me.LBL_JOSS.TabIndex = 21
+        Me.LBL_JOSS.Text = "                                                INTEGRANTES:" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "PASPUEL DILAN, CHAN" &
+    "GOLUISA JEFFERSON, CUMBAJIN ALEX, " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "VILLAMARIN PAULO, LAGLA ERICK, QUISIMALIN JO" &
+    "CELINE"
+        '
+        'LBL_TECNOLOGIAS
+        '
+        Me.LBL_TECNOLOGIAS.AutoSize = True
+        Me.LBL_TECNOLOGIAS.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.LBL_TECNOLOGIAS.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LBL_TECNOLOGIAS.Location = New System.Drawing.Point(312, 95)
+        Me.LBL_TECNOLOGIAS.Name = "LBL_TECNOLOGIAS"
+        Me.LBL_TECNOLOGIAS.Size = New System.Drawing.Size(319, 19)
+        Me.LBL_TECNOLOGIAS.TabIndex = 20
+        Me.LBL_TECNOLOGIAS.Text = "UNIDAD DE GESTIÓN DE TECNOLOGÍAS"
+        '
+        'LBL_UNIVERSIDAD
+        '
+        Me.LBL_UNIVERSIDAD.AutoSize = True
+        Me.LBL_UNIVERSIDAD.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.LBL_UNIVERSIDAD.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LBL_UNIVERSIDAD.Location = New System.Drawing.Point(282, 60)
+        Me.LBL_UNIVERSIDAD.Name = "LBL_UNIVERSIDAD"
+        Me.LBL_UNIVERSIDAD.Size = New System.Drawing.Size(383, 19)
+        Me.LBL_UNIVERSIDAD.TabIndex = 19
+        Me.LBL_UNIVERSIDAD.Text = "UNIVERSIDAD DE LAS FUERZAS ARMADAS ESPE"
+        '
+        'Presentacion
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(912, 479)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.LBL_EJERCICIOS)
+        Me.Controls.Add(Me.LBL_NRC)
+        Me.Controls.Add(Me.LBL_INGENIERO)
+        Me.Controls.Add(Me.LBL_REDES)
+        Me.Controls.Add(Me.LBL_PROGRAMACION)
+        Me.Controls.Add(Me.LBL_JOSS)
+        Me.Controls.Add(Me.LBL_TECNOLOGIAS)
+        Me.Controls.Add(Me.LBL_UNIVERSIDAD)
+        Me.Name = "Presentacion"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents LBL_EJERCICIOS As Label
+    Friend WithEvents LBL_NRC As Label
+    Friend WithEvents LBL_INGENIERO As Label
+    Friend WithEvents LBL_REDES As Label
+    Friend WithEvents LBL_PROGRAMACION As Label
+    Friend WithEvents LBL_JOSS As Label
+    Friend WithEvents LBL_TECNOLOGIAS As Label
+    Friend WithEvents LBL_UNIVERSIDAD As Label
+End Class
